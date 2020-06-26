84c3f0deed5de0d96ead1063c211509869b4e43a
jc durbin <jcdurbin@hotmail.com>
2020-06-26 16:10:25 +0100

    remove parse array example

--- examples/scripts/init.js
+++ examples/scripts/init.js
@@ -2,7 +2,7 @@ import loader from "./loader.js";
 
 export default (() => {
   loader("parseAndValidateString", "parseAndValidateString");
-  loader("parseAndValidateArray", "parseAndValidateArray");
+
   loader("getDestinationPoint", "getDestinationPoint");
   loader("getIntersectionPoint");
 
